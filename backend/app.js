2df67c29d7f5d88667556f1da9f36b015e3e1fd9
Nebil Muhidin <nebilmuhidin57@gmail.com>
2024-03-30 06:23:30 +0300

    connect the database

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,17 @@
+require('dotenv').config()
 const express = require('express')
 const CORS = require('cors')
+const connectDB = require('./database/db');
 
+const port = process.env.PORT || 3000
+const uri = process.env.MONGO_URI
 const app = express()
 
 app.use(express.json())
-
 app.use(CORS())
+
+app.listen(port,()=>{
+    connectDB(uri)
+    .then(()=>console.log(`Connected Sucessfully and listening on port ${port}`))
+    .catch((e)=>console.log(e.message))
+})
